85488fec18cf0464cb1423c0ccc1ed21261b4074
Pedro H Almeida <pedroalmeida173@gmail.com>
2015-11-13 07:01:59 -0200

    - Alteracao index.php para colocar rodapé dinamico - Criacao do arquivo contato.php

---
+++ contato.php
@@ -0,0 +1,24 @@
+<h1>Contato</h1>
+
+<form role="form" action="index.php?conteudo=mostracontato.php">
+    <div class="form-group">
+        <label for="nome">Nome:</label>
+        <input type="nome" class="form-control" id="nome">
+    </div>
+
+    <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" class="form-control" id="email">
+    </div>
+
+    <div class="form-group">
+        <label for="assunto">Assunto:</label>
+        <input type="assunto" class="form-control" id="assunto">
+    </div>
+
+    <div class="form-group">
+        <label for="mensagem">Mensagem:</label>
+        <input type="mensagem" class="form-control" id="mensagem">
+    </div>
+    <button type="submit" class="btn btn-default">Enviar</button>
+</form>
--- index.php
+++ index.php
@@ -37,5 +37,16 @@
         ?>
 
     </div>
+
+    <br><br>
+
+    <center>
+        <p>
+            <?php
+            date_default_timezone_set('UTC');
+            echo "Todos os direitos reservados - ".date('Y');
+            ?>
+        </p>
+    </center>
 </body>
 </html>
