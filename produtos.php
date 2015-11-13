c2d8fe8078e9418c0871f0534e68825ea09ea6ca
Pedro H Almeida <pedroalmeida173@gmail.com>
2015-11-13 06:39:31 -0200

    - Criando arquivo produtos.php - Criando arquivo servicos.php

---
+++ produtos.php
@@ -0,0 +1,3 @@
+<h1>Produtos</h1>
+
+<p>Nossos produtos são inovadores no mercado.</p>
---
+++ servicos.php
@@ -0,0 +1,5 @@
+<h1>Serviços</h1>
+
+<p>Com moderna infraestrutura, a empresa atende às necessidades específicas de cada cliente, com responsabilidade social respeito ao meio ambiente.
+    Em 12 anos de atuação no mercado, investe sempre em equipamentos de ponta e na capacitação profissional para aumentar a qualidade e a
+    eficiência de seus serviços e garantir a satisfação de seus clientes.</p>
